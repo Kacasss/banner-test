4f2d781ae592b1d7712ad9ce74ea5556913e4bc6
Kacasss <pppsssce1@gmail.com>
2023-02-06 00:00:12 +0900

    桜祭りをメインに画像修正、桜祭り3の横バージョンを追加

---
+++ main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+
+// window.addEventListener("load", () => {
+//   console.log("ロードされました");
+
+//   const ulCherryBlossomFestival = document.getElementById("cherry-blossom-festival");
+//   const ulTokyoSunflowerFestival = document.getElementById("tokyo-sunflower-festival");
+//   const ulTokyoSunflowerFestivalLandscape = document.getElementById("tokyo-sunflower-festival_landscape");
+  
+//   for (let i = 0; i < 4; i++) {
+//     const li = document.createElement("li");
+//   }
+
+// });
